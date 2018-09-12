9fc01ee664fa20bf4712e702afa872760d2433f1
Jimmy Berry <jimmy@boombatower.com>
2018-09-11 22:14:12 -0500

    Provide bash completion script.

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ INSTALL = install
 INSTALL_PROGRAM = $(INSTALL)
 
 PREFIX = /usr
+SYSCONFIG = /etc
 VERSION="build-$(shell date +%F)"
 
 all:
@@ -15,3 +16,6 @@ install:
 	$(INSTALL_PROGRAM) tumbleweed $(DESTDIR)$(PREFIX)/bin/
 	sed -i 's/VERSION=".*"/VERSION=$(VERSION)/' \
 	  $(DESTDIR)$(PREFIX)/bin/tumbleweed
+
+	$(INSTALL) -d $(DESTDIR)$(SYSCONFIG)/bash_completion.d
+	$(INSTALL) tumbleweed-completion.bash $(DESTDIR)$(SYSCONFIG)/bash_completion.d
---
+++ tumbleweed-completion.bash
@@ -0,0 +1,43 @@
+#/usr/bin/env bash
+
+_tumbleweed_completion()
+{
+  if [ ${#COMP_WORDS[@]} -eq 2 ] ; then
+    COMPREPLY=($(compgen -W "history init installed latest list migrate revert status switch target uninit unmigrate update upgrade version --version --help" -- "${COMP_WORDS[-1]}"))
+  elif [[ ${#COMP_WORDS[@]} -gt 2 && (
+      "${COMP_WORDS[1]}" == "init" ||
+      "${COMP_WORDS[1]}" == "switch" ||
+      "${COMP_WORDS[1]}" == "update" ||
+      "${COMP_WORDS[1]}" == "upgrade" ) ]] ; then
+
+    local flags=("--force" "--install")
+    for word in ${COMP_WORDS[@]:2} ; do
+      for i in ${!flags[@]} ; do
+        if [ "${flags[$i]}" == "$word" ] ; then
+          unset flags[$i]
+        fi
+      done
+    done
+    COMPREPLY=($(compgen -W "$(echo ${flags[@]})" -- "${COMP_WORDS[-1]}"))
+
+    if [ "${COMP_WORDS[1]}" == "switch" ] ; then
+      # Cache the list to avoid remote call each completion.
+      if [ -z "$_TUMBLEWEED_LIST" ] ; then
+        _TUMBLEWEED_LIST="$(tumbleweed list)"
+      fi
+
+      # Skip list if a snapshot is already an argument.
+      for word in ${COMP_WORDS[@]:2} ; do
+        for snapshot in $_TUMBLEWEED_LIST ; do
+          if [ "$snapshot" == "$word" ] ; then
+            return
+          fi
+        done
+      done
+
+      COMPREPLY+=($(compgen -W "$_TUMBLEWEED_LIST" -- "${COMP_WORDS[-1]}"))
+    fi
+  fi
+}
+
+complete -F _tumbleweed_completion tumbleweed
